d64e79fee5627753176422ef976b1447e20ebc79
star001-code <starzeki001@gmail.com>
2025-07-21 07:13:17 +0300

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,8 @@
+function playMemory() {
+  const audio = document.getElementById("memoryAudio");
+  audio.play();
+}
 
+function sendMessage() {
+  alert("الرسالة الأخيرة: لن تُنسى، حتى وإن اختفيتِ.");
+}
